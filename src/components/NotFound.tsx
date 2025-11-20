198417ef4c6cada25e61f08e8f8e1c16a195d0db
rodrigo010101 <rodrigo.iac87@gmail.com>
2025-11-19 21:20:40 -0300

    v.0.2.2

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,9 @@
+function NotFound(){
+    return (
+        <div style = {{textAlign : "center",marginTop : "50px"}}>
+        <h1>404 - pagina no encontrada</h1>
+        <p>La ruta que busca no existe.</p>
+        </div>
+    );
+}
+export default NotFound;
