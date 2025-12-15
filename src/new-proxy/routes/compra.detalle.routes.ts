7c0b28196998b22a735dd7cc4a7a1385a93ddcfa
rodrigo0187 <ro.aedo@duocuc.cl>
2025-12-15 06:13:54 -0300

    v.0.7.4

--- src/new-proxy/routes/compra.detalle.routes.ts
+++ src/new-proxy/routes/compra.detalle.routes.ts
@@ -1,4 +1,4 @@
-import {Router} from "express";
+import { Router } from "express";
 import verifyToken from "../middlewares/verifyToken";
 // llamada de metodos que contiene el controller
 import {
@@ -6,20 +6,20 @@ import {
     getCompraDetalleById,
     createCompraDetalle,
     getCompraDetalleByIdUser
-}from "../controllers/compra.detalle.controller";
+} from "../controllers/compra.detalle.controller";
 
 
 const router = Router();
 // crear detalle
-router.post("/compra-detalle",verifyToken,createCompraDetalle);
+router.post("/compra-detalle", verifyToken, createCompraDetalle);
 
 // todos los detalles de la compra
-router.get("/compra-detalle",verifyToken,getAllcompraDetalle);
+router.get("/compra-detalle", verifyToken, getAllcompraDetalle);
 
 //obtener detalle compra por id
-router.get("/compra-detalle/compra/:id_compra",verifyToken,getCompraDetalleById);
+router.get("/compra-detalle/compra/:id_compra", verifyToken, getCompraDetalleById);
 
 // obtener el detalle compra por usuario id
-router.get("/compra-detalle/mis-detalles",verifyToken,getCompraDetalleByIdUser);
+router.get("/compra-detalle/mis-detalles", verifyToken, getCompraDetalleByIdUser);
 
 export default router;
